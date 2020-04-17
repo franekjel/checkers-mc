dd7ee46bd46147263b8f9cd32d37fa1fe6a4280f
Franciszek Jełowicki <franek.jelowicki@gmail.com>
2020-04-17 21:59:42 +0200

    Server init

--- cmcserver/cmcserver.go
+++ cmcserver/cmcserver.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
+	"os/exec"
+	"strings"
 
+	flag "github.com/spf13/pflag"
+)
+
+func main() {
+	checkersPath := flag.StringP("path", "p", "./checkers-mc", "path to checkers-mc executable")
+	flag.Parse()
+	checkExecutable(*checkersPath)
+}
+
+func checkExecutable(path string) {
+	cmd := exec.Command(path, "-h")
+	out, err := cmd.CombinedOutput()
+	if err != nil && err.(*exec.ExitError).ExitCode() != 1 {
+		log.Fatal("Error launching ", path, ": ", err.Error())
+	}
+	if !strings.Contains(string(out), "find best move in checkers") {
+		log.Print(path, " -h  output is:\n", string(out))
+		log.Fatal("Program ", path, " is not checkers-mc program")
+		os.Exit(1)
+	}
+	log.Print("Program ", path, " seems to be checkers-mc")
 }
--- cmcserver/go.mod
+++ cmcserver/go.mod
@@ -1,3 +1,5 @@
 module cmcserver
 
 go 1.14
+
+require github.com/spf13/pflag v1.0.5
